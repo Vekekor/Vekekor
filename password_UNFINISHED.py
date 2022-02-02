47998f66f6f1e9378a312bcd97e280341090a739
Vekekor <98216494+Vekekor@users.noreply.github.com>
2022-02-02 23:22:44 +0100

    Add files via upload

---
+++ password_UNFINISHED.py
@@ -0,0 +1,35 @@
+from tkinter import *
+from time import sleep
+root = Tk()
+windwo =  root.geometry("300x350")
+name = StringVar()
+password = StringVar()
+def correct_username():
+    if name.get() == "Homer77":
+        return True
+    elif name.get() != "Homer77":
+        print("False")
+def correct_password():
+    if password.get() == "Homerissecret":
+        return True
+    elif password.get() !="Homerissecret":
+        print("False")
+getin = False
+def check_if_bothCorrect():
+    if correct_username() and correct_password() == True:
+        print("Gaemoor")
+            
+    else:
+        print("Try again")
+     
+
+
+Label(root, text="Enter username").pack(pady=5)
+Entry(root, textvariable=name).pack(pady=5)
+
+Label(root, text="Enter password").pack(pady=5)
+Entry(root, textvariable=password,show="*").pack(pady=5)
+Button(root, command=check_if_bothCorrect,text="Log in").pack(pady=9)
+
+
+root.mainloop()
